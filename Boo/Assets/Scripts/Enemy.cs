6b4160467feb04a33c90b325ead5cf7b6ed293c9
mythguy1226 <robbiereddick@aol.com>
2021-09-26 15:11:49 -0400

    Started Basic Enemy AI

---
+++ Boo/Assets/Scripts/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Movement
+{
+    // Fields for Enemy
+    public GameObject player;
+    public List<Vector3> path;
+    int pathIndex;
+    bool pathReversed;
+
+    // Method at start
+    protected override void Start()
+    {
+        // Init fields
+        pathIndex = 0;
+        pathReversed = false;
+        base.Start();
+    }
+
+    // Method for Steering the Zombie
+    protected override void CalculateSteeringForces()
+    {
+        // Start force at 0
+        Vector3 ultimateForce = Vector3.zero;
+
+        // Make the enemy move towards the first coordinate of the path
+        // if a path exists
+        if(path.Count > 0)
+        {
+            ultimateForce += Seek(path[pathIndex]);
+        }
+        // Clamp the magnitude then apply the final force
+        ultimateForce = Vector3.ClampMagnitude(ultimateForce, 1);
+        ApplyForce(ultimateForce);
+    }
+
+    // Use Base Update plus adding the new updates
+    protected override void Update()
+    {
+        // Check if the enemy has reached one of its paths
+        if(Vector3.Distance(transform.position, path[pathIndex]) <= 0.1f)
+        {
+            // If the path is reversed then subtract from index
+            // if at end of path then reverse direction again
+            if(pathReversed)
+            {
+                if(pathIndex - 1 >= 0)
+                {
+                    pathIndex--;
+                }
+                else
+                {
+                    pathReversed = false;
+                    pathIndex++;
+                }
+            }
+            // If the path isnt reversed then add to the index
+            // if at end of path then reverse direction again
+            else
+            {
+                if (pathIndex + 1 < path.Count)
+                {
+                    pathIndex++;
+                }
+                else
+                {
+                    pathReversed = true;
+                    pathIndex--;
+                }
+            }
+        }
+
+        base.Update();
+    }
+}
---
+++ Boo/Assets/Scripts/Movement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movement : MonoBehaviour
+{
+    // Physics fields
+    protected Vector3 position;
+    protected Vector3 direction;
+    protected Vector3 velocity;
+
+    [Min(0.0001f)]
+    public float mass = 1;
+    public float maxSpeed = 10;
+
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        position = transform.position;
+        direction = Vector3.right;
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        CalculateSteeringForces();
+
+        position += velocity * Time.deltaTime;
+
+        transform.position = position;
+
+        // Grab our current direction from velocity
+        direction = velocity.normalized;
+    }
+
+    // Method for applying forces on an object
+    public void ApplyForce(Vector3 force)
+    {
+        velocity += force / mass;
+    }
+
+    // Method for getting the steering force for seeking
+    public Vector3 Seek(Vector3 targetPos)
+    {
+        // Calculate Desired Velocity
+        Vector3 desiredVelocity = targetPos - position;
+
+        // Set Desired equal to the max speed
+        desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+        // Calculate the seek steering force
+        Vector3 seekingForce = desiredVelocity - velocity;
+
+        // Return the seek steering force
+        return seekingForce;
+    }
+
+    // Second overload method for seeking where you 
+    // take an object as a parameter
+    public Vector3 Seek(GameObject target)
+    {
+        // Return the seek steering force
+        return Seek(target.transform.position);
+    }
+
+    // Abstract Method for Calculating the Steering Forces
+    protected abstract void CalculateSteeringForces();
+}
