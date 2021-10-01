3875c7847f063d5d2796474c3f50626731c1c435
kwiatkowskiandrewj <59908612+kwiatkowskiandrewj@users.noreply.github.com>
2021-10-01 01:15:15 -0400

    Finished Vampire Movement
    Added a Scene for testing out stuff.

---
+++ Boo/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Fields
+
+    public float speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement
+        #region
+
+        // Moves Up
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+
+        // Moves Down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.up * speed * Time.deltaTime);
+        }
+
+        // Moves Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        // Moves Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-Vector3.right * speed * Time.deltaTime);
+        }
+
+        #endregion
+    }
+}
