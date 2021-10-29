ed5a71f4a3b7b61128c6c53465534a0d8cf736c8
MistTheDork <54997585+MistTheDork@users.noreply.github.com>
2021-10-28 22:30:41 -0400

    Revert "Merge branch 'main' into Animation"
    This reverts commit cf22115cd59453d1db040bf6d2f8b24e9e475614, reversing
    changes made to 74bc94a3c7a8c650619f81235cced26fd3fdcb01.

--- Boo/Assets/Scripts/UI/UI_Health.cs
+++ Boo/Assets/Scripts/UI/UI_Health.cs
@@ -4,43 +4,36 @@ using UnityEngine;
 
 public class UI_Health : MonoBehaviour
 {
-    // UI Fields
+
     public GameObject heartContainer;
     public GameObject GameManager;
     [SerializeField] GameObject heartOBJ;
     [SerializeField] GameObject brokenHeartOBJ;
 
-    // Method that draws the hearts
+
     public void DrawHearts(int numberHearts, int maxHealth)
     {
-        // Destroy all objects to reset the UI state
         foreach(Transform t in this.transform)
         {
             Destroy(t.gameObject);
         }
-
-        // Initial X-Spacer
-        float spacer = 0.5f;
-
-        // Iterate through the amount of hearts
+        float spacer = 1;
         for (int i = 0; i < maxHealth; i++)
         {
-            // Typical transform for all heart elements before being spaced
-            Vector3 tempTransform = new Vector3(transform.position.x * spacer, transform.position.y - 30, transform.position.z);
 
-            // Set Heart Objects
+            Vector3 tempTransform = new Vector3(transform.position.x * spacer, transform.position.y, transform.position.z);
+
             if (i +1 <= numberHearts)
             {
-                GameObject g = Instantiate(heartOBJ, tempTransform, Quaternion.identity);
+                 GameObject g = Instantiate(heartOBJ, tempTransform, Quaternion.identity);
                 g.transform.parent = transform;
-                spacer += 0.5f; // Add space between elements
+                spacer += .3f;
             }
-            // Set Broken Heart Objects
             else
             {
                 GameObject g = Instantiate(brokenHeartOBJ,tempTransform, Quaternion.identity);
                 g.transform.parent = transform;
-                spacer += 0.5f; // Add space between elements
+                spacer += .3f;
             }
         }
 
